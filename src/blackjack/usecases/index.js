1d11ee1ff2df40c356a3d1cfff0241dd9d0a46e1
kyodar <npaezquintana@gmail.com>
2023-09-16 20:55:53 -0500

    First Commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export  {crearDeck} from './crear-deck';
+export { valorCarta } from './valor-carta';
+export {pedirCarta} from './pedir-carta';
+export {turnoComputadora} from './turno-computadora';
+export {crearCartaHTML} from './crear-carta-html';
---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @param {Array<String>} deck 
+ * @returns {String} retorna una carta
+ */
+
+export const  pedirCarta =(deck) =>{
+    
+    
+    if (!deck || deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
